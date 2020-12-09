28a111a776d9d248b9ec08cbb936c3bc284c2288
Gerome Heath <gheath@travelrepublic.co.uk>
2020-12-09 05:43:13 +0000

    Login typegraphql

---
+++ src/types/MyContext.ts
@@ -0,0 +1,15 @@
+
+import { Request } from "express";
+
+export interface MyContext {
+    req: Request & {
+      session: {
+        userId?: any;
+      };
+    };
+    res: Response & {
+      session: {
+        userId?: any;
+      };
+    };
+  }
